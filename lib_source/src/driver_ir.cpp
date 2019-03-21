c94e435592e84ec6f753ecbc0e8eec19a756c251
Carlos Eduardo Alves Sarmento <edusrmt@A102-12L.imd.local>
2019-03-21 12:16:49 -0300

    negate, min e reverse implementados

---
+++ lib_source/src/driver_ir.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // std :: cout , std :: endl
+#include <iterator> // std :: begin () , std :: end ()
+#include "../include/int_ranges.h"
+using namespace std;
+
+int main () {
+    // Inserir teste aqui
+
+    return 0;
+}
--- lib_source/src/int_ranges.cpp
+++ lib_source/src/int_ranges.cpp
@@ -1,16 +1,23 @@
 #include "../include/int_ranges.h"
 
 namespace ir {
-    void negate( value_type * first, value_type * last )
+    void negate(value_type * first, value_type * last)
     {
-        while( first != last )
-            *first++ *= 1;
+        while(first != last)
+            *first++ *= -1;
     }
 
-    const value_type * min( const value_type* first, const value_type* last )
-    {
-        // TODO: place your code solution here!
-        return nullptr;
+    const value_type * min(const value_type* first, const value_type* last)
+    {   
+        const value_type *min = first++;
+        while (first != last) {
+            if (*first < *min)
+                min = first;
+
+            ++first;
+        }
+
+        return min;
     }
 
     value_type * copy( const value_type * first_, const value_type *last_, value_type *d_first_ )
@@ -19,9 +26,14 @@ namespace ir {
         return nullptr;
     }
 
-    void reverse( value_type *first, value_type*last )
+    void reverse(value_type *first, value_type*last)
     {
-        // TODO: place your code solution here!
+        --last;
+        while (first < last) {
+            std::swap(*first, *last);
+            ++first;
+            --last;
+        }
     }
 
     value_type * rotate( value_type *first, value_type *n_first, value_type *last )
