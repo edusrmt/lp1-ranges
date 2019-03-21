a6a995978b4375e67b8be2ed8fcf5ebd5e5f50c0
Carlos Eduardo Alves Sarmento <edusrmt@A102-12L.imd.local>
2019-03-21 11:17:07 -0300

    Arquivos do professor

---
+++ lib_source/src/int_ranges.cpp
@@ -0,0 +1,36 @@
+#include "../include/int_ranges.h"
+
+namespace ir {
+    void negate( value_type * first, value_type * last )
+    {
+        while( first != last )
+            *first++ *= 1;
+    }
+
+    const value_type * min( const value_type* first, const value_type* last )
+    {
+        // TODO: place your code solution here!
+        return nullptr;
+    }
+
+    value_type * copy( const value_type * first_, const value_type *last_, value_type *d_first_ )
+    {
+        // TODO: place your code solution here!
+        return nullptr;
+    }
+
+    void reverse( value_type *first, value_type*last )
+    {
+        // TODO: place your code solution here!
+    }
+
+    value_type * rotate( value_type *first, value_type *n_first, value_type *last )
+    {
+        // TODO: place your code solution here!
+        return nullptr;
+    }
+
+    // TODO: Place the rest of the implementation here.
+
+
+}
